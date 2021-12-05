174a4742c1227bd931acbc4ce793ac1a3a20b312
Nadeem Lari <nadeemlari@gmail.com>
2021-12-05 13:47:47 +0530

    C

---
+++ Publisher/Program.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Messaging.WebPubSub;
+
+const string connectionString = "Endpoint=https://izaan.webpubsub.azure.com;AccessKey=7+rZAXhhRR+eXQqZ3Mby1dNsulbY0JMQIeO2NdZnDXE=;Version=1.0;";
+const string hub = "izaan.webpubsub.azure.com";
+var client = new WebPubSubServiceClient(connectionString,hub);
+while (true)
+{
+    Console.WriteLine("Enter message");
+    var msg = Console.ReadLine();
+    await client.SendToAllAsync(msg);
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Channels;
+using Azure.Messaging.WebPubSub;
+using Websocket.Client;
+
+const string connectionString = "Endpoint=https://izaan.webpubsub.azure.com;AccessKey=7+rZAXhhRR+eXQqZ3Mby1dNsulbY0JMQIeO2NdZnDXE=;Version=1.0;";
+const string hub = "izaan.webpubsub.azure.com";
+var client = new WebPubSubServiceClient(connectionString,hub);
+var url = client.GetClientAccessUri();
+using var ws = new WebsocketClient(url);
+ws.MessageReceived.Subscribe(msg =>
+{
+    Console.WriteLine($"Message received: {msg}");
+    
+});
+await ws.Start();
+Console.WriteLine("I am connected");
+Console.Read();
